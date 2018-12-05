de503c5e21562d56e13550ec291564938b96856c
liujianghao <jianghaoliu@jusstech.com>
2018-12-05 16:48:11 +0800

    update

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ $(function(){
 	
 	function expand(){
 		var ok = false;
-		var e = $('.hide-article-box');
+		var e = $('.check-adblock-bg');
+
 		if(e.length > 0){
 			ok = true;
 			e.remove();
+			$('.hide-article-box').remove();
 			$('#article_content').removeAttr('style');
+			for(var i = 1; i < 1000; i++) {
+					clearTimeout(i);
+			}
+			// $('.check-adblock-bg').remove();
 		}
 		
 		console.log("expand", ok, count);
